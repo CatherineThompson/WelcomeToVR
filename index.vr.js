85edae47544e3a1075118c4d8dc307747e366710
Catherine Thompson <cathythompson0915@gmail.com>
2017-04-26 11:25:08 -0600

    position canon and ship

--- index.vr.js
+++ index.vr.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import {
   AppRegistry,
   asset,
@@ -6,11 +6,17 @@ import {
   Pano,
   Text,
   View,
-  Sphere,
   Model
 } from 'react-vr'
 
 export default class WelcomeToVR extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      angle: 0
+    }
+  }
+
   render () {
     return (
       <View style={{}}>
@@ -20,26 +26,47 @@ export default class WelcomeToVR extends React.Component {
             obj: asset('canon_jouet.obj'),
             mtl: asset('canon_jouet.mtl')
           }}
-          style={{position: 'absolute', transform: [{translate: [0, 0, -6]}]}}
-        />
-        <Text
           style={{
-            backgroundColor: 'orange',
-            fontSize: 0.5,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          Cathy's Test App
-        </Text>
-
+            position: 'absolute',
+            transform: [
+              { translate: [0, -2, -1.5] },
+              { rotateX: this.state.angle },
+              { rotateY: -90 },
+              { scale: 0.13 }
+            ]
+          }}
+        />
+        <Model
+          source={{
+            obj: asset('Pirate Ship.obj'),
+            mtl: asset('Pirate Ship.mtl')
+          }}
+          style={{
+            position: 'absolute',
+            transform: [
+              { translate: [0, -2, -200] },
+              { scale: 0.01 }
+            ]
+          }}
+        />
       </View>
     )
   }
 }
 
 AppRegistry.registerComponent('WelcomeToVR', () => WelcomeToVR)
+
+/* <Text
+  style={{
+    backgroundColor: 'orange',
+    fontSize: 0.5,
+    fontWeight: '400',
+    layoutOrigin: [0.5, 0.5],
+    paddingLeft: 0.2,
+    paddingRight: 0.2,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    transform: [{translate: [0, 0, -3]}],
+  }}>
+  Cathy's Test App
+</Text> */
